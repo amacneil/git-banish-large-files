1fc1fb0a92d2060dfefb11adbb48eb72d114a554
Adrian Macneil <adrian.macneil@gmail.com>
2016-07-06 16:25:00 -0700

    Create pre-receive.sh

---
+++ pre-receive.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -eu
+
+nullsha="0000000000000000000000000000000000000000"
+maxsize=5
+maxbytes=$(( $maxsize * 1024 * 1024 ))
+status=0
+
+# Read stdin for ref information
+while read oldref newref refname; do
+  # Skip branch deletions
+  if [ "$newref" = "$nullsha" ]; then
+    continue
+  fi
+
+  # Set oldref to HEAD if this is branch creation
+  if [ "$oldref" = "$nullsha" ]; then
+    oldref="HEAD"
+  fi
+
+  # Find large objects
+  for file in $(git rev-list --objects ${oldref}..${newref} | \
+      git cat-file --batch-check='%(objectname) %(objecttype) %(objectsize) %(rest)' | \
+      awk -v maxbytes="$maxbytes" '$3 > maxbytes { print $4 }'); do
+
+    # Display error header if this is the first offending file
+    if [ "$status" -eq "0" ]; then
+      status=1
+
+      echo ""
+      echo "-------------------------------------------------------------------------"
+      echo "Your push was rejected because it contains files larger than $maxsize MB."
+      echo "Please discuss with the infra team the best place to store these files."
+      echo "You might want to consider using https://git-lfs.github.com/ instead."
+      echo "-------------------------------------------------------------------------"
+      echo
+      echo "Offending files:"
+    fi
+
+    echo " - $file"
+  done
+done
+
+if [ "$status" -ne "0" ]; then echo; fi
+exit $status
